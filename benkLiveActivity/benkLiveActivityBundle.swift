4d16b870e746caf7bb181d086a7c2843032c767c
Vyson KOH <vysonk18@gmail.com>
2025-12-18 15:10:41 +0800

    Fixed quests.

---
+++ benkLiveActivity/benkLiveActivityBundle.swift
@@ -0,0 +1,18 @@
+//
+//  benkLiveActivityBundle.swift
+//  benkLiveActivity
+//
+//  Created by Vyson KOH on 18/12/2025.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct benkLiveActivityBundle: WidgetBundle {
+    var body: some Widget {
+        benkLiveActivity()
+        benkLiveActivityControl()
+        benkLiveActivityLiveActivity()
+    }
+}
